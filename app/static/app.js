9f6d0e7943e4149f1b13394a7fc73617b7c325a0
Hao Chen <hchen1202@gmail.com>
2017-03-08 23:16:38 -0500

    initialize React FrontEnd

---
+++ app/static/app.js
@@ -0,0 +1,11 @@
+/**
+* @Author: Hao Chen <clovemac>
+* @Date:   2017-03-08T23:05:27-05:00
+* @Email:  hao@genm.co
+* @Project: GenmMobile
+* @Last modified by:   clovemac
+* @Last modified time: 2017-03-08T23:05:27-05:00
+*/
+
+
+
---
+++ app/static/routes.js
@@ -0,0 +1,11 @@
+/**
+* @Author: Hao Chen <clovemac>
+* @Date:   2017-03-08T23:05:47-05:00
+* @Email:  hao@genm.co
+* @Project: GenmMobile
+* @Last modified by:   clovemac
+* @Last modified time: 2017-03-08T23:05:47-05:00
+*/
+
+
+
